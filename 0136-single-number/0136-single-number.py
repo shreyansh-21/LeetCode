64f772beba19a10d8e3cf0c9362e6986d3968c8d
Shreyansh Dubey <143270324+shreyansh-21@users.noreply.github.com>
2025-05-21 01:14:45 +0530

    Time: 7 ms (33.73%), Space: 20 MB (9.67%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        c = Counter(nums)
+        for k,v in c.items():
+            if v == 1:
+                return k
+        return 0
+        
