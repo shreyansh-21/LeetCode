0158525c9f8150629d433e0c0541d1f37b63b27e
Shreyansh Dubey <143270324+shreyansh-21@users.noreply.github.com>
2026-01-04 15:07:39 +0530

    Time: 195 ms (54.01%), Space: 18.9 MB (58.88%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.py
+++ 1390-four-divisors/1390-four-divisors.py
@@ -2,26 +2,16 @@
 
 class Solution:
     def sumFourDivisors(self, nums: List[int]) -> int:
-        total_sum = 0
-
+        total = 0
         for n in nums:
             divisors = set()
-            i = 1
-
-            while i * i <= n:
+            for i in range(1,int(math.sqrt(n)+1)):
                 if n % i == 0:
                     divisors.add(i)
                     divisors.add(n // i)#aisa karne se ek hi time pe do mil jaten hain aur i*i wala logic sustain karta hai
-
-                # early break: agar 4 se zyada ho gaye
+                # early break: agar 4 se zyada ho gaye    
                 if len(divisors) > 4:
                     break
-
-                i += 1
-
             if len(divisors) == 4:
-                total_sum += sum(divisors)
-
-        return total_sum
-
-        
+                total += sum(divisors)
+        return total
