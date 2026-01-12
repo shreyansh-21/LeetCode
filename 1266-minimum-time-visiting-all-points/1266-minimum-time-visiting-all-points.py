a10d758eb2954d3ee268d2bd9bbf2b51d29233a8
Shreyansh Dubey <143270324+shreyansh-21@users.noreply.github.com>
2026-01-12 22:50:16 +0530

    Time: 4 ms (15%), Space: 19.4 MB (6.71%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.py
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.py
@@ -2,6 +2,9 @@ class Solution:
     def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
         ans = 0
         for i in range(1, len(points)):
+            # dx = |x2 - x1|
+            # dy = |y2 - y1|
+            # So the minimum time to move between two points is simply:max(|x2 - x1|, |y2 - y1|)
             ans += max(
                 abs(points[i][0] - points[i - 1][0]),
                 abs(points[i][1] - points[i - 1][1])
