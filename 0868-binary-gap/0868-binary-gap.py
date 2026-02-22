d268a7485f0b5e2fc526041b7a91404b65782ab5
Shreyansh Dubey <143270324+shreyansh-21@users.noreply.github.com>
2026-02-22 08:54:22 +0530

    Time: 0 ms (100%), Space: 19.3 MB (55.21%) - LeetHub

--- 0868-binary-gap/0868-binary-gap.py
+++ 0868-binary-gap/0868-binary-gap.py
@@ -1,15 +1,16 @@
 class Solution:
     def binaryGap(self, n: int) -> int:
-        b=bin(n)[2:]
-        if b.count('1')<=1:
+        binary_str = bin(n)[2:]
+        if binary_str.count('1') <= 1:
             return 0
-        c=0
-        m=0
-        for i in b:
-            if i=='0':
-                c+=1
-            elif i=='1':
-                m=max(m,c+1)
-                c=0
-        return m
         
+        current_zero_count = 0
+        max_gap = 0
+        
+        for bit in binary_str:
+            if bit == '0':
+                current_zero_count += 1
+            elif bit == '1':
+                max_gap = max(max_gap, current_zero_count + 1)
+                current_zero_count = 0
+        return max_gap
